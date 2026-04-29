496074c7edddd37e9ed15b108fa818f6f6f98d3c
harshith1123 <somethingincredible123@gmail.com>
2026-04-29 10:25:36 +0530

    React First Day Learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import SideBar from './SideBar';
+import Props from './Props';
+
+let name = "Harshit Shetty"
+let car_obj = {
+  name: "Jaguar",
+  color: "Red"
+}
+
+let a = 19;
+let msg = "";
+if(a>10){
+  msg = "Hello!...."
+}
+else{
+  msg = "Bye!..."
+}
+const friends = ["Pavan","Vishal","Harsha"]
 
 function App() {
   return (
+    <>
+    {/* <h1>Content Starts </h1>
+    <h3>{msg}</h3>
+    <h3>{a<10 ? a + " is less than 10" : a + "is greater than 10"}</h3>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>{name}</h1>
+      <h1>{5+8}</h1>
+      <h1>{car_obj.name}</h1>
+      <h1>{car_obj.color}</h1>
+    </div> */}
+    {/* <Header/>
+    <SideBar/>
+    <Footer/> */}
+    {/* <Props name = "Harshit" age = "22">
+      <p>Children Element 1</p>
+      <button type='button' value = "Click">Click 1</button>
+    </Props> */}
+    <Props age = {a} isMarried = {false} arr={friends}>
+      <p>Children Element 2</p>
+      <button type='button' value = "Click">Click 2</button>
+    </Props>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Header from './Header';
+import Footer from './Footer';
+import SideBar from './SideBar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // <>
+  //   <Header/>
+  //   <Footer/>
+  //   <SideBar/>
+  // </>
   <React.StrictMode>
     <App />
   </React.StrictMode>
