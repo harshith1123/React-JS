a8de84fcc76d1d8b3cc91ad0f2781b53e68de279
harshith1123 <somethingincredible123@gmail.com>
2026-05-04 20:33:25 +0530

    Understood List key concept in React

--- src/List1.js
+++ src/List1.js
@@ -3,38 +3,55 @@ import List2 from './List2';
 
 export default function List1() {
     const students = ["Harshit","Shreyata","Pavan","Vishal"];
-    const result = students.map(std => <li>{std}</li>)
+    const result = students.map((std,index) =><li key={index}>{std}</li>)
     const list = [
         {
+            id:1,
             name: "Harshit",
             age: 22
         },
         {
+            id:2,
             name: "Shreyata",
             age: 18
         },
         {
+            id:3,
             name: "Pavan",
             age: 23
         },
         {
+            id:4,
             name: "Vishal",
             age: 22
         }
     ];
-    const res = list.map((arr)=><p>Hey!... Name:<span>{arr.name}</span> and age is: <span>{arr.age}</span></p>);
+    // const res = list.map((arr)=><p>Hey!... Name:<span>{arr.name}</span> and age is: <span>{arr.age}</span></p>);
+    const res = list.map((arr)=><li key={arr.id}>Hey!... Name:{arr.name} and age is: {arr.age}</li>);
   return (
     <div>
         <h1>Directly Showing</h1>
-        {students.map(std => <li>{std}</li>)}
+        <ul>
+            {students.map((std,index) => <li key={index}>{std}</li>)}
+        </ul>
+        
         <h1>Showing after storing in array</h1>
-        {result}
+        <ul>
+            {result}
+        </ul>
+        
 
         <h1>Showing Objects</h1>
-        {res}
+        <ul>
+            {res}
+        </ul>
+        
 
         <h1>Passing as Props</h1>
-        {list.map(arr => <List2 list={arr}/>)}
+        <ul>
+            {list.map(arr => <List2 list={arr}/>)}
+        </ul>
+        
     </div>
   )
 }
--- src/List2.js
+++ src/List2.js
@@ -1,14 +1,16 @@
 import React from 'react'
 
 // export default function List2(props) {
-export default function List2({list}) {
+export default function List2({list,key}) {
   return (
     <div>
     {/* <h1>Direct printing through props</h1> */}
       {/* {<p>Hey!... Name:<span>{props.list.name}</span> and age is: <span>{props.list.age}</span></p>} */}
 
-      {/* <h1>Through Destructing</h1> */}
+      <h1>Through Destructing</h1>
     {<p>Hey!... Name:<span>{list.name}</span> and age is: <span>{list.age}</span></p>}
+    {/* {key} */}
+    {/* {<li>Hey!... Name:{list.name} and age is: {list.age}</li>} */}
     </div>
   )
 }
