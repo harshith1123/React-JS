5fb2b7af8f9e8ffbae0a3069b45ed9d8cdce2fe7
harshith1123 <somethingincredible123@gmail.com>
2026-05-08 23:22:06 +0530

    Remove misplaced src and public from root

--- src/Arrow_Functions.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Arrow = props => <h1>Hey {props.name}</h1>
-
-export default Arrow;
--- src/SideBar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SideBar(){
-    return <h1>This is the SideBar</h1>
-}
-
-export default SideBar;
