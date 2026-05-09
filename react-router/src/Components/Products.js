72c8bc603679cac09bc58f854c7515c559a8aed8
harshith1123 <somethingincredible123@gmail.com>
2026-05-09 23:17:24 +0530

    Learned Dynamic Routing in React

--- react-router/src/Components/Products.js
+++ react-router/src/Components/Products.js
@@ -8,8 +8,13 @@ function Products() {
     <>
         <div className='products'>
             <h1>This is the Products Component</h1>
-            <Link to={"shirts"}>Shirts</Link>
-            <Link to={"pants"}>Pants</Link>
+            {/* Below two are the Relative Path */}
+            {/* <Link to={"shirts"}>Shirts</Link>
+            <Link to={"pants"}>Pants</Link> */}
+
+            {/* Below two are the absolute path */}
+            <Link to={"/products/shirts"}>Shirts</Link>
+            <Link to={"/products/pants"}>Pants</Link>
         </div>
         {/* Allows to display one of the child components when any one of the link by clicking */}
         <Outlet/>    
