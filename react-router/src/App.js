0d2e4607815af765474f1fe9be3d58f4586ad2b3
harshith1123 <somethingincredible123@gmail.com>
2026-05-09 15:38:04 +0530

    Learned Navigating Programming invarious scenarios

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './Components/Home';
 import About from './Components/About';
 import Contact from './Components/Contact';
@@ -8,6 +8,26 @@ import Linking from './Components/Linking';
 import NavLinking from './Components/NavLinking';
 
 function App() {
+  const navigate = useNavigate();
+  // const navigateToAbout = ()=>{
+  //   navigate('/about');
+  // }
+
+  // Event Handling navigation in single function
+  // const navigateTo = (url) =>{
+  //   navigate(url);
+  // }
+
+  // Through Condition
+  const navigateTo = () =>{
+    const a = true;
+    if(a){
+      navigate('/about')
+    }
+    else{
+      navigate('/contact')
+    }
+  }
   return (
     <div className="App">
     {/* <Linking/> */}
@@ -17,6 +37,21 @@ function App() {
         <Route path= '/about' element={<About/>}/>
         <Route path = '/contact' element={<Contact/>}/>
       </Routes>
+      {/* <button type="submit" onClick={()=>navigate('/about') }>About</button> */}
+      {/* <button type="submit" onClick={()=>navigate('/contact') }>Contact</button> */}
+
+      {/* Using Event Handler Function */}
+      {/* <button type="submit" onClick={()=> navigateToAbout() }>About</button> */}
+{/* 
+      <button type="submit" onClick={()=> navigateTo('/about') }>About</button>
+      <button type="submit" onClick={()=> navigateTo('/contact') }>Contact</button> */}
+
+      
+      <button type="submit" onClick={()=> navigateTo('/about') }>About</button>
+      <button type="submit" onClick={()=> navigateTo('/contact') }>Contact</button>
+
+      {/* To Go Back */}
+      <button type="submit" onClick={()=> navigate(-1) }>Go Back</button>
     </div>
   );
 }
