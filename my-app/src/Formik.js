3c1a0aa816982b5a7b536edfba78b601091017c0
harshith1123 <somethingincredible123@gmail.com>
2026-05-11 10:56:23 +0530

    Performed Formik by Restructuring the variable

--- my-app/src/Formik.js
+++ my-app/src/Formik.js
@@ -8,7 +8,17 @@ function Formik() {
         email:''
     }
 
-    const formik =useFormik({
+    // const formik =useFormik({
+    //     initialValues:formikInitialValue,
+    //     onSubmit: (values)=>{
+    //         console.log(values);
+    //         console.log(values.name);
+    //         console.log(values.email);
+    //         alert("Your Name:"+values.name+ " and Email:"+values.email+ " saved successfully");
+    //     }
+    // });
+
+    const {handleChange,handleSubmit,values} =useFormik({
         initialValues:formikInitialValue,
         onSubmit: (values)=>{
             console.log(values);
@@ -20,13 +30,17 @@ function Formik() {
 
   return (
     <div>
-      <form action="" onSubmit={formik.handleSubmit}>
+    <h1>Formik Demo</h1>
+      {/* <form action="" onSubmit={formik.handleSubmit}> */}
+      <form action="" onSubmit={handleSubmit}>
         <label htmlFor="">Enter Name:</label>
-        <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange}/>
+        {/* <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange}/> */}
+        <input type="text" name="name" value={values.name} onChange={handleChange}/>
         <br />
         <br />
         <label htmlFor="">Enter Email:</label>
-        <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange}/>
+        {/* <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange}/> */}
+        <input type="email" name="email" value={values.email} onChange={handleChange}/>
         <br />
         <br />
         <input type="submit" value="Submit" />
