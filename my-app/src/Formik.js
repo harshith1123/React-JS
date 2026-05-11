97a62b90911b53a2cba0734738b2db5c68102a54
harshith1123 <somethingincredible123@gmail.com>
2026-05-11 18:48:48 +0530

    Learned some more validation using Formik and Yup Library

--- my-app/src/Formik.js
+++ my-app/src/Formik.js
@@ -6,7 +6,10 @@ function Formik() {
 
     const formikInitialValue = {
         name:'',
-        email:''
+        email:'',
+        age:'',
+        pass:'',
+        confPass:''
     }
 
     // const formik =useFormik({
@@ -19,14 +22,18 @@ function Formik() {
     //     }
     // });
 
-    const {handleChange,handleSubmit,values,errors} =useFormik({
+    const {handleChange,handleSubmit,handleBlur,values,errors,touched} =useFormik({
         initialValues:formikInitialValue,
         validationSchema: YupSchema,
-        onSubmit: (values)=>{
+        onSubmit: (values,action)=>{
             console.log(values);
             console.log(values.name);
             console.log(values.email);
-            alert("Your Name:"+values.name+ " and Email:"+values.email+ " saved successfully");
+            console.log(values.age);
+            console.log(values.pass);
+            console.log(values.confPass);
+            alert("Your Name:"+values.name+ " , Email:"+values.email+ " , Age:" + values.age +" with password" + " saved successfully");
+            action.resetForm()
         }
     });
 
@@ -37,15 +44,32 @@ function Formik() {
       <form action="" onSubmit={handleSubmit}>
         <label htmlFor="">Enter Name:</label>
         {/* <input type="text" name="name" value={formik.values.name} onChange={formik.handleChange}/> */}
-        <input type="text" name="name" value={values.name} onChange={handleChange}/>
+        <input type="text" name="name" value={values.name} onChange={handleChange} onBlur={handleBlur}/>
         <br />
-        <span style={{color:"red"}}>{errors.name}</span>
+        {/* <span style={{color:"red"}}>{errors.name}</span> */}
+        {errors.name && touched.name ? <span style={{color:"red"}}>{errors.name}</span> : null}
         <br />
         <label htmlFor="">Enter Email:</label>
         {/* <input type="email" name="email" value={formik.values.email} onChange={formik.handleChange}/> */}
-        <input type="email" name="email" value={values.email} onChange={handleChange}/>
+        <input type="email" name="email" value={values.email} onChange={handleChange} onBlur={handleBlur}/>
         <br />
-        <span style={{color:"red"}}>{errors.email}</span>
+        {/* <span style={{color:"red"}}>{errors.email}</span> */}
+        {errors.email && touched.email ? <span style={{color:"red"}}>{errors.email}</span> : null}
+        <br />
+        <label htmlFor="">Enter Age:</label>
+        <input type="text" name="age" value={values.age} onChange={handleChange} onBlur={handleBlur}/>
+        <br />
+        {errors.age && touched.age ? <span style={{color:"red"}}>{errors.age}</span> : null}
+        <br />
+        <label htmlFor="">Enter Password:</label>
+        <input type="password" name="pass" value={values.pass} onChange={handleChange} onBlur={handleBlur} />
+        <br />
+        {errors.pass && touched.pass ? <span style={{color:"red"}}>{errors.pass}</span> : null}
+        <br />
+        <label htmlFor="">Confirm Password:</label>
+        <input type="password" name="confPass" value={values.confPass} onChange={handleChange} onBlur={handleBlur} />
+        <br />
+        {errors.confPass && touched.confPass ? <span style={{color:"red"}}>{errors.confPass}</span> : null}
         <br />
         <input type="submit" value="Submit" />
       </form>
--- my-app/src/YupSchema.js
+++ my-app/src/YupSchema.js
@@ -2,7 +2,10 @@ import * as Yup from 'yup';
 
 export const YupSchema = Yup.object({
     // name:Yup.string().min(3).max(15).required(), --> will throw default error message
-    name:Yup.string().min(3, "Min 3 characters are allowed").max(15, "Max 15 characters are allowed").required("Name Input is Required"),
+    name:Yup.string().min(3, "Min 3 characters are allowed").max(15, "Max 15 characters are allowed").required("Name Field is Required"),
     // email:Yup.string().email().required()
-    email:Yup.string().email("Invalid Email Typed, Please Enter a valid Email").required("Email is Required")
+    email:Yup.string().email("Invalid Email Typed, Please Enter a valid Email").required("Email Field is Required"),
+    age:Yup.number("Only Number Type is allowed").min(10).max(50).required("Age Field is Required"),
+    pass:Yup.string().required().matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/, "Enter Strong Password"),
+    confPass:Yup.string().required().oneOf([Yup.ref('pass'),null],"Password Doesn't Matches")
 })
